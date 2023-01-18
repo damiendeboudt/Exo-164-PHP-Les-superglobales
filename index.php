000f4873f72a86e9e5ddda8b3eff2ee6f798adfb
Damien <damien.deboudt@sfr.fr>
2023-01-18 09:29:35 +0100

    ok

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
+echo $_SERVER['PHP_SELF'] . "<br>";
+
+echo $_SERVER['SERVER_NAME'] . "<br>";
+
+echo $_SERVER['SERVER_ADDR'] . "<br>";
+
+echo $_SERVER['REMOTE_ADDR'] . "<br>";
+
+echo $_SERVER['HTTPS'] . "<br>";
+
+echo $_SERVER['REQUEST_TIME'] . "<br>";
+
+echo $_SERVER['GATEWAY_INTERFACE'] . "<br>";
+
+echo $_SERVER['REQUEST_TIME_FLOAT'] . "<br>";
 
 /**
  * A l'aide de la documentation officielle de PHP.
  * 1. Retrouvez la page traitant des superglobales pour $_SERVER
- * 2. Récupérez un maximum de valeurs de ce tableau et affichez les à l'aide d'un simple echo ( il n'est pas interdit d'être intelligent, c'est un tableau associatif... )
+ * 2. Récupérez un maximum de valeurs de ce tableau et affichez les à l'aide d'un simple echo
+ * ( il n'est pas interdit d'être intelligent, c'est un tableau associatif... )
  * 3. Uploadez ce projet sur votre serveur et testez !
  */
 // TODO Votre code ici
